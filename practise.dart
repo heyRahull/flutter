4562cab5c5e9f771a9a3c1abfac6004d5139e51f
Rahul Agarwal (INFOSYS LIMITED) <v-rahulagarw@microsoft.com>
2025-08-22 12:12:07 +0530

    First Commit

---
+++ practise.dart
@@ -0,0 +1,22 @@
+void main() {
+  // print("Hello Rahul, this is my test Dart file!");
+  // var a =10;
+  // print(a);
+
+  // int num = 1384108;
+  // String name = "Rahul Agarwal";
+  // print("My name is $name and my employee id is : $num");
+
+  // var roll_num = 38;
+  // roll_num = 40;
+  // print(roll_num);
+
+  // var name;
+  // name="agarwal";
+  // name=90;
+  // print(name);
+
+  for(int i=0; i<10; i++){
+    print("My name is Rahul Agarwal.");
+  }
+}
