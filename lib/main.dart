a9845917fac943a563cea894d595bdecf9fd7031
Rahul Agarwal (INFOSYS LIMITED) <v-rahulagarw@microsoft.com>
2025-08-22 12:23:47 +0530

    Exercise on Center Container

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,21 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("My First Flutter Container")),
-      //  body: Text("Hello World"),
-      body: Center(
-        child : Text("Hello World")
-      )
+        //  Exercise on Center Widget - Create a container, make it center and 
+        //  add a text and make it center of container.
+        body: Center(
+          child: Container(
+            width: 200,
+            height: 150,
+            color: Colors.black87,
+            child: Center(
+              child: Text(
+                "Rahul Agarwal",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
